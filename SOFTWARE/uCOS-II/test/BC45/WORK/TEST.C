1b3a6d706cef8112792afae81b0095234a67b3f8
5fe6eb50c7aa19f9 <51sf1hgd@gmail.com>
2022-03-18 22:31:33 +0800

    [test] init

--- SOFTWARE/uCOS-II/test/BC45/WORK/TEST.C
+++ SOFTWARE/uCOS-II/test/BC45/WORK/TEST.C
@@ -3,16 +3,15 @@
 *                                                uC/OS-II
 *                                          The Real-Time Kernel
 *
-*                          (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
+*                           (c) Copyright 1992-2002, Jean J. Labrosse, Weston, FL
 *                                           All Rights Reserved
 *
-*                                               EXAMPLE #4
+*                                               EXAMPLE #1
 *********************************************************************************************************
 */
 
 #include "includes.h"
 
-/*$PAGE*/
 /*
 *********************************************************************************************************
 *                                               CONSTANTS
@@ -30,7 +29,8 @@
 
 OS_STK        TaskStk[N_TASKS][TASK_STK_SIZE];        /* Tasks stacks                                  */
 OS_STK        TaskStartStk[TASK_STK_SIZE];
-INT8U         TaskData[N_TASKS];                      /* Parameters to pass to each task               */
+char          TaskData[N_TASKS];                      /* Parameters to pass to each task               */
+OS_EVENT     *RandomSem;
 
 /*
 *********************************************************************************************************
@@ -44,7 +44,7 @@ static  void  TaskStartCreateTasks(void);
 static  void  TaskStartDispInit(void);
 static  void  TaskStartDisp(void);
 
-/*$PAGE*/
+/*$PAGE*/
 /*
 *********************************************************************************************************
 *                                                MAIN
@@ -60,19 +60,13 @@ void  main (void)
     PC_DOSSaveReturn();                                    /* Save environment to return to DOS        */
     PC_VectSet(uCOS, OSCtxSw);                             /* Install uC/OS-II's context switch vector */
 
-    OSTaskCreateExt(TaskStart,
-                    (void *)0,
-                    &TaskStartStk[TASK_STK_SIZE - 1],
-                    0,                                     /* Task priority = 0                        */
-                    0,
-                    &TaskStartStk[0],
-                    TASK_STK_SIZE,
-                    (void *)0,
-                    OS_TASK_OPT_SAVE_FP);
+    RandomSem   = OSSemCreate(1);                          /* Random number semaphore                  */
+
+    OSTaskCreate(TaskStart, (void *)0, &TaskStartStk[TASK_STK_SIZE - 1], 0);
     OSStart();                                             /* Start multitasking                       */
 }
 
-/*$PAGE*/
+
 /*
 *********************************************************************************************************
 *                                              STARTUP TASK
@@ -82,8 +76,7 @@ void  TaskStart (void *pdata)
 {
 #if OS_CRITICAL_METHOD == 3                                /* Allocate storage for CPU status register */
     OS_CPU_SR  cpu_sr;
-#endif    
-    INT8U      i;
+#endif
     char       s[100];
     INT16S     key;
 
@@ -102,19 +95,21 @@ void  TaskStart (void *pdata)
     TaskStartCreateTasks();                                /* Create all the application tasks         */
 
     for (;;) {
-        TaskStartDisp();
+        TaskStartDisp();                                  /* Update the display                       */
+
 
         if (PC_GetKey(&key) == TRUE) {                     /* See if key has been pressed              */
             if (key == 0x1B) {                             /* Yes, see if it's the ESCAPE key          */
-                PC_DOSReturn();                            /*      Return to DOS                       */
+                PC_DOSReturn();                            /* Return to DOS                            */
             }
         }
 
-        OSCtxSwCtr = 0;
+        OSCtxSwCtr = 0;                                    /* Clear context switch counter             */
         OSTimeDlyHMSM(0, 0, 1, 0);                         /* Wait one second                          */
     }
 }
-/*$PAGE*/
+
+/*$PAGE*/
 /*
 *********************************************************************************************************
 *                                        INITIALIZE THE DISPLAY
@@ -128,10 +123,10 @@ static  void  TaskStartDispInit (void)
     PC_DispStr( 0,  0, "                         uC/OS-II, The Real-Time Kernel                         ", DISP_FGND_WHITE + DISP_BGND_RED + DISP_BLINK);
     PC_DispStr( 0,  1, "                                Jean J. Labrosse                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     PC_DispStr( 0,  2, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-    PC_DispStr( 0,  3, "                                    EXAMPLE #4                                  ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  3, "                                    EXAMPLE #1                                  ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     PC_DispStr( 0,  4, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-    PC_DispStr( 0,  5, "TaskPrio      Angle   cos(Angle)   sin(Angle)                                   ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-    PC_DispStr( 0,  6, "--------      -----   ----------   ----------                                   ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  5, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+    PC_DispStr( 0,  6, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     PC_DispStr( 0,  7, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     PC_DispStr( 0,  8, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
     PC_DispStr( 0,  9, "                                                                                ", DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
@@ -177,7 +172,7 @@ static  void  TaskStartDisp (void)
     sprintf(s, "%5d", OSCtxSwCtr);                                 /* Display #context switches per second */
     PC_DispStr(18, 23, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
 
-    sprintf(s, "V%4.2f", (float)OSVersion() * 0.01);               /* Display uC/OS-II's version number    */
+    sprintf(s, "V%1d.%02d", OSVersion() / 100, OSVersion() % 100); /* Display uC/OS-II's version number    */
     PC_DispStr(75, 24, s, DISP_FGND_YELLOW + DISP_BGND_BLUE);
 
     switch (_8087) {                                               /* Display whether FPU present          */
@@ -209,21 +204,11 @@ static  void  TaskStartDisp (void)
 static  void  TaskStartCreateTasks (void)
 {
     INT8U  i;
-    INT8U  prio;
 
 
     for (i = 0; i < N_TASKS; i++) {                        /* Create N_TASKS identical tasks           */
-        prio        = i + 1;
-        TaskData[i] = prio;
-        OSTaskCreateExt(Task,
-                        (void *)&TaskData[i],
-                        &TaskStk[i][TASK_STK_SIZE - 1],
-                        prio,
-                        0,
-                        &TaskStk[i][0],
-                        TASK_STK_SIZE,
-                        (void *)0,
-                        OS_TASK_OPT_SAVE_FP);
+        TaskData[i] = '0' + i;                             /* Each task will display its own letter    */
+        OSTaskCreate(Task, (void *)&TaskData[i], &TaskStk[i][TASK_STK_SIZE - 1], i + 1);
     }
 }
 
@@ -235,27 +220,18 @@ static  void  TaskStartCreateTasks (void)
 
 void  Task (void *pdata)
 {
-    FP32   x;
-    FP32   y;
-    FP32   angle;
-    FP32   radians;
-    char   s[81];
-    INT8U  ypos;
+    INT8U  x;
+    INT8U  y;
+    INT8U  err;
 
 
-    ypos  = *(INT8U *)pdata + 7;
-    angle = (FP32)(*(INT8U *)pdata) * (FP32)36.0;
     for (;;) {
-        radians = (FP32)2.0 * (FP32)3.141592 * angle / (FP32)360.0;
-        x       = cos(radians);
-        y       = sin(radians);
-        sprintf(s, "   %2d       %8.3f  %8.3f     %8.3f", *(INT8U *)pdata, angle, x, y);
-        PC_DispStr(0, ypos, s, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
-        if (angle >= (FP32)360.0) {
-            angle  =   (FP32)0.0;
-        } else {
-            angle +=   (FP32)0.01;
-        }
-        OSTimeDly(1);
+        OSSemPend(RandomSem, 0, &err);           /* Acquire semaphore to perform random numbers        */
+        x = random(80);                          /* Find X position where task number will appear      */
+        y = random(16);                          /* Find Y position where task number will appear      */
+        OSSemPost(RandomSem);                    /* Release semaphore                                  */
+                                                 /* Display the task number on the screen              */
+        PC_DispChar(x, y + 5, *(char *)pdata, DISP_FGND_BLACK + DISP_BGND_LIGHT_GRAY);
+        OSTimeDly(1);                            /* Delay 1 clock tick                                 */
     }
 }
